58df8e105441de0dda007d82f0944ac34ca8e4a0
sivachelvan-T <rdhsiva07@gmail.com>
2026-09-15 23:18:35 -0400

    fix project folders

---
+++ dummy-backend/dummy-backend.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const port = 3000;
+const app = express();
+
+app.get("/api/message", (req, res) => {
+    res.json("welcome to dummy backend");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
